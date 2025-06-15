d6ee883cbe42d2bd00b021dd3ddd66078b4a3714
Rizky Purnama Sidiq <rpurnamasidiq@gmail.com>
2025-06-15 09:54:22 +0700

    menghilangkan redundan if (instance of ClientServer) pada handler.js

--- src/server.js
+++ src/server.js
@@ -1,12 +1,12 @@
-//* Server adalah entry pointnya, karena server.js lah yang dijalankan pertama kali oleh node menggunakan node src/server.js
-
 const Hapi = require('@hapi/hapi');
-const notes = require('./api/notes'); //! Secara default, Jika Hapi diberikan nama folder nya saja seperti "notes", ia akan mencari index.js. Karena index.js adalah default entry pointnya
+const notes = require('./api/notes');
 const NotesService = require('./services/inMemory/NoteService');
 const NotesValidator = require('./validator/notes');
- 
+const ClientError = require('./exceptions/ClientError'); // Tambahkan ini
+
 const init = async () => {
-  const notesService = new NotesService(); //* Best practice! Soalnya instance ini hanya dipanggil sekali di dalam function itu sendiri. Jadi tidak perlu disimpan diluar function, cakupannya cukup lokal saja
+  const notesService = new NotesService();
+
   const server = Hapi.server({
     port: 3000,
     host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
@@ -16,17 +16,33 @@ const init = async () => {
       },
     },
   });
- 
+
   await server.register({
-    plugin: notes, //* Notes ini hanya merujuk pada file yang memiliki plug-in object
+    plugin: notes,
     options: {
       service: notesService,
-      validator: NotesValidator 
+      validator: NotesValidator,
     },
   });
- 
+
+  // 💡 Tambahkan extension function di sini
+  server.ext('onPreResponse', (request, h) => {
+    const { response } = request;
+
+    if (response instanceof ClientError) {
+      const newResponse = h.response({
+        status: 'fail',
+        message: response.message,
+      });
+      newResponse.code(response.statusCode);
+      return newResponse;
+    }
+
+    return h.continue;
+  });
+
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
 };
- 
+
 init();
