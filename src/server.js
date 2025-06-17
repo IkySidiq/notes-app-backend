b80d28e86031afb6df7644173735d3d6cf30a02d
Rizky Purnama Sidiq <rpurnamasidiq@gmail.com>
2025-06-17 13:28:27 +0700

    Membuat Tabel Notes dengan Teknik Migrate

--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
-require('dotenv').config(); 
+require('dotenv').config(); //* Otomatis mengacu pada .env secara default
 
 const Hapi = require('@hapi/hapi');
 const notes = require('./api/notes');
 const NotesService = require('./services/inMemory/NoteService');
 const NotesValidator = require('./validator/notes');
-const ClientError = require('./exceptions/ClientError'); // Tambahkan ini
+const ClientError = require('./exceptions/ClientError');
 
 const init = async () => {
   const notesService = new NotesService();
